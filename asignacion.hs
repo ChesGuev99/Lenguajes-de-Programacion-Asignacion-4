708ec3e8f569749993a9c45ef1e6e419907cd2ef
ChesGuev99 <anaguevarabonilla@gmail.com>
2021-11-24 00:06:27 -0600

    Maybe close

--- asignacion.hs
+++ asignacion.hs
@@ -6,7 +6,7 @@ data Proposition =
     | Disyuncion   Proposition Proposition
     | Implicacion  Proposition Proposition
     | Equivalencia Proposition Proposition
-    deriving Eq
+    deriving (Eq, Show)
 
 
 imprimir :: Proposition -> String
@@ -14,13 +14,20 @@ imprimir (Constante    False)     = "False"
 imprimir (Constante    True)      = "True"
 imprimir (Variable     var)       = var   
 imprimir (Negacion     expr)      = "~"
-imprimir (Conjuncion   exp1 exp2) = '(' : show(exp1) ++ " /\\" ++ " " ++ show(exp2) ++ ")"
--- show exp1 ++ "/\\" ++ exp2
-imprimir (Disyuncion   exp1 exp2) = "\\/" exp1 exp2
-imprimir (Implicacion  exp1 exp2) = "=>" exp1 exp2
-imprimir (Equivalencia exp1 exp2) = "<=>" exp1 exp2
+imprimir (Conjuncion   prop1 prop2) = imprimirPropAux "/\\" prop1 prop2
+imprimir (Disyuncion   prop1 prop2) = imprimirPropAux "\\/" prop1 prop2
+imprimir (Implicacion  prop1 prop2) = imprimirPropAux "=>" prop1 prop2
+imprimir (Equivalencia prop1 prop2) = imprimirPropAux "<=>" prop1 prop2
 
+
+imprimirPropAux :: String -> Proposition -> Proposition -> String
+imprimirPropAux operation prp1 prp2= show(prp1) ++ " " ++ operation ++ " " ++ show(prp2)
+
+aa = True :: Bool
+--a = aa :: Proposition
 
 main =
+    -- todavia no se como probarlo, pero creeeeo que funciona ;-;
+    imprimir Negacion(aa)
     putStrLn "Hello World"  
 
