4b2e680ed8faf4d0ff12ad2dcf54e9e75022a032
ChesGuev99 <anaguevarabonilla@gmail.com>
2021-11-24 17:52:39 -0600

    full sintax?

--- asignacion.hs
+++ asignacion.hs
@@ -1,33 +1,33 @@
-data Proposition =  
+-- data Var = Var Char
+--     deriving (Eq, Show)
+
+data Proposition =       
       Constante    Bool
     | Variable     String
-    | Negacion     String
+    | Negacion     Proposition
     | Conjuncion   Proposition Proposition
     | Disyuncion   Proposition Proposition
     | Implicacion  Proposition Proposition
     | Equivalencia Proposition Proposition
     deriving (Eq, Show)
 
-
 imprimir :: Proposition -> String
-imprimir (Constante    False)     = "False"
-imprimir (Constante    True)      = "True"
-imprimir (Variable     var)       = var   
-imprimir (Negacion     expr)      = "~"
-imprimir (Conjuncion   prop1 prop2) = imprimirPropAux "/\\" prop1 prop2
-imprimir (Disyuncion   prop1 prop2) = imprimirPropAux "\\/" prop1 prop2
-imprimir (Implicacion  prop1 prop2) = imprimirPropAux "=>" prop1 prop2
-imprimir (Equivalencia prop1 prop2) = imprimirPropAux "<=>" prop1 prop2
+imprimir (Constante    False)       = "False"
+imprimir (Constante    True)        = "True"
+imprimir (Variable     var)         = var   
+imprimir (Negacion     prop1)       = "~" ++ imprimir prop1
+imprimir (Conjuncion   prop1 prop2) = "Conjuncion (" ++ imprimirPropAux "/\\" prop1 prop2 ++ ")"
+imprimir (Disyuncion   prop1 prop2) = "Disyuncion (" ++ imprimirPropAux "\\/" prop1 prop2 ++ ")"
+imprimir (Implicacion  prop1 prop2) = "Implicacion (" ++ imprimirPropAux "=>" prop1 prop2 ++ ")"
+imprimir (Equivalencia prop1 prop2) = "Equivalencia (" ++ imprimirPropAux "<=>" prop1 prop2 ++ ")"
 
 
 imprimirPropAux :: String -> Proposition -> Proposition -> String
-imprimirPropAux operation prp1 prp2= show(prp1) ++ " " ++ operation ++ " " ++ show(prp2)
+imprimirPropAux operation prp1 prp2= imprimir(prp1) ++ " " ++ operation ++ " " ++ imprimir(prp2)
 
-aa = True :: Bool
---a = aa :: Proposition
 
-main =
-    -- todavia no se como probarlo, pero creeeeo que funciona ;-;
-    imprimir Negacion(aa)
-    putStrLn "Hello World"  
+a :: Proposition
+a = Conjuncion (Variable "A") (Variable "B")
 
+main = print (imprimir a)
+   -- putStrLn "Hello World"  
