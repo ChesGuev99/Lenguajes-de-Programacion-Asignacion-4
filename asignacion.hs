c2c0c42bf64607bb9143c962ee797925b2af8dd0
Valeria Sell <valeria_sell@outlook.com>
2021-11-27 08:31:56 -0600

    bonita 2.0
    is this this end?

--- asignacion.hs
+++ asignacion.hs
@@ -6,6 +6,7 @@ import AsVals
 import Gen_bools
 import EvalProp
 import Fnd
+import Bonita
   
 -- import Data.Map (Map, fromList, lookup)
 
@@ -16,12 +17,12 @@ a :: Proposition
 aa :: Proposition
 b :: Proposition
 c = Negacion(Constante True)
-a = Conjuncion (Constante True) (Variable "B")
+--Sa = Conjuncion (Constante True) (Variable "B")
 --a = Conjuncion (Variable "C") (Variable "B")
 b = Disyuncion (Variable "A") (Variable "B")
 aa = Equivalencia b a
 -- a = Negacion a
--- a = Negacion(Implicacion(Conjuncion (Constante True) (Variable "B")) (Disyuncion (Variable "A") (Variable "B")))
+a = Negacion(Implicacion(Conjuncion (Constante True) (Variable "B")) (Disyuncion (Variable "A") (Variable "B")))
 len = 4
 
 listaB :: [Bool]
@@ -34,5 +35,5 @@ r = "vars -> "++(imprimir c) ++ "  ->   " ++ show(vars c)
 s = "gen_bools -> "++ show(len) ++ "  ->   " ++ show(gen_bools len) 
 t = "as_vals -> " ++ show(vars aa) ++ " + " ++ show(listaB) ++ "  ->   " ++ show(as_vals (vars aa) listaB) 
 
-main = print (deMorgan aa)
+main = print (bonita a)
    -- putStrLn "Hello World"  
--- bonita.hs
+++ bonita.hs
@@ -1,1 +1,23 @@
 module Bonita where
+
+import Sintax
+
+bonita :: Proposition -> String
+bonita (Constante     True)      =  "True"
+bonita (Constante     False)      =  "False"
+bonita (Variable      prop1)      =  prop1
+bonita (Negacion      prop1)      = '~' : bonita prop1
+bonita (Conjuncion    prop1 prop2)= bonitaAux "/\\" prop1 prop2
+bonita (Disyuncion   prop1 prop2) = bonitaAux "\\/" prop1 prop2
+bonita (Implicacion   prop1 prop2) = bonitaAux "->" prop1 prop2
+bonita (Equivalencia prop1 prop2) = bonitaAux "<->" prop1 prop2
+    
+--showOperador :: String -> Proposition -> Proposition -> String
+--showOperador operador prop1 prop2= "(" ++ (bonita prop1) ++ operador ++ (bonita prop2) ++ ")"
+
+muestraConectivos :: (Proposition -> String) -> String -> Proposition -> Proposition -> String
+muestraConectivos show_ conectivo prop1 prop2 =
+  '(' : show_ prop1 ++ " " ++ conectivo ++ " " ++ show_ prop2 ++ ")"
+
+bonitaAux :: String -> Proposition -> Proposition -> String
+bonitaAux = muestraConectivos bonita
