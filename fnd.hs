3104893ae0d76d0e9d94367afcb1e2efffabff43
ChesGuev99 <anaguevarabonilla@gmail.com>
2021-11-27 03:56:53 -0600

    comentarios

--- fnd.hs
+++ fnd.hs
@@ -2,15 +2,20 @@ module Fnd where
 import Sintax
 import EvalProp
 
+--operador de negacion
 neg :: Proposition -> Proposition
 neg = Negacion
 
+--operador de disyuncion
 disj :: Proposition -> Proposition -> Proposition
 disj = Disyuncion
 
+--operador de conjuncion
 conj :: Proposition -> Proposition -> Proposition
 conj = Conjuncion
 
+--Funcion encargada de realizar la forma normal disyuntiva, a traves de las correspondientes reglas logicas
+-- hace uso de los operadores previos 
 fnd :: Proposition -> Proposition
 fnd (Constante prop)                     = Constante prop
 fnd (Negacion(Constante prop))           = Constante (not prop)
@@ -31,8 +36,11 @@ fnd (Equivalencia prop1 prop2)            = fnd $ implicacionNeg prop1 prop2
 
 fnd (Negacion (Equivalencia prop1 prop2)) = fnd $ implicacionNeg prop1 prop2
 
+
+-- Funcion encargada de llevar a cabo la negacion de una implicacion
 implicacionNeg :: Proposition -> Proposition-> Proposition
 implicacionNeg a b = (a `conj` b) `disj` (neg a `conj` neg b)
 
+-- Funcion encargada de llevar a cabo la negacion de una equivalencia
 equivalenciaNeg :: Proposition -> Proposition-> Proposition
 equivalenciaNeg a b = (a `disj` b) `conj` (a `disj` b)
--- taut.hs
+++ taut.hs
@@ -10,10 +10,13 @@ import Data.List
 -- incrementar :: Int -> Int --POR QUÉ HASKELL ES TAN SHIT QUE NO PUEDE INCREMENTAR COMO UN LENGUAJE NORMAL
 -- incrementar i = i + 1 --(╯°□°）╯
 
-
+--Funcion encargada de evaluar si la proposicion es verdadera o falsa
 toEvalProp :: Proposition -> [(String, Bool)] -> Bool 
 toEvalProp prop vals = evalProp prop vals
 
+
+--Funcion encargada de recorrer las diferentes asignaciones de valores, 
+-- finalmente, enviara una lista vacia si es una tautologia o la lista de valores que causaron la falacia
 recorrer :: Proposition -> [[Bool]]-> [[Bool]]
 recorrer _ [[]] = []
 recorrer prop lista = 
@@ -30,6 +33,8 @@ recorrer prop lista =
                     recorrer prop (drop 1 lista)
         else [head (lista)]
 
+--Funcion principal encargada de diagnosticar una tautologia, en caso de que reciba que es una falacia, 
+--imprime los valores dados, caso contrario, indica que es tautologia
 taut :: Proposition -> String
 taut prop = 
     let 
@@ -43,27 +48,6 @@ taut prop =
         else
             imprimir prop ++ "  no es una tautologia, por que  " ++ show (as_vals variables (head result))
     
--- taut :: Proposition -> Indice -> Bool 
--- taut prop indice = let
---     variables = vars prop
---     combinaciones = gen_bools 
---     indice = length combinaciones --COMO MODIFICAMOS ESTO CONSTANTEMENTE??
---     sublista = toEvalProp combinaciones indice 
---     asociacion = as_vals variables sublista
---     evaluacion_es_verdadera = evalProp prop asociacion
---         in
---             if evaluacion_es_verdadera
---                 incrementar indice 
---                      --NO ME GUSTA LA PROGRAMACION FUNCIONAL     
---                      --ESTO ME PASA POR QUEDARME DORMIDA EN LAS CLASES DE NACHITO ｡ﾟ(TᐞT)ﾟ
---                 if indice == length combinaciones
---                     in True
---                 taut prop indice --ESTO ES LEGAL?
---             else
---                 in False
---                      --retorna false y vamonos  
---                  --CHES COMO RETORNO EVALUACION_ES_VERDADERA??? o(´Д｀*)o
-
 -- propTaut = Implicacion (Conjuncion (Variable "A") (Variable "B")) (Variable "A")
 -- a = Constante True
 
