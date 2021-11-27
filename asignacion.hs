316e06fc368087c1299c42dd48a8594654aa6025
ChesGuev99 <anaguevarabonilla@gmail.com>
2021-11-27 03:43:00 -0600

    pruebas?

--- asignacion.hs
+++ asignacion.hs
@@ -6,6 +6,8 @@ import AsVals
 import Gen_bools
 import EvalProp
 import Fnd
+import Bonita
+import Taut
   
 -- import Data.Map (Map, fromList, lookup)
 
@@ -15,7 +17,7 @@ import Fnd
 a :: Proposition
 aa :: Proposition
 b :: Proposition
-c = Negacion(Constante True)
+c = Constante True
 a = Conjuncion (Constante True) (Variable "B")
 --a = Conjuncion (Variable "C") (Variable "B")
 b = Disyuncion (Variable "A") (Variable "B")
@@ -23,6 +25,8 @@ aa = Equivalencia b a
 -- a = Negacion a
 -- a = Negacion(Implicacion(Conjuncion (Constante True) (Variable "B")) (Disyuncion (Variable "A") (Variable "B")))
 len = 4
+
+propTaut = Implicacion (Conjuncion (Variable "A") (Variable "B")) (Variable "A")
 
 listaB :: [Bool]
 listaA = vars aa
@@ -33,6 +37,10 @@ test = as_vals listaA listaB
 r = "vars -> "++(imprimir c) ++ "  ->   " ++ show(vars c) 
 s = "gen_bools -> "++ show(len) ++ "  ->   " ++ show(gen_bools len) 
 t = "as_vals -> " ++ show(vars aa) ++ " + " ++ show(listaB) ++ "  ->   " ++ show(as_vals (vars aa) listaB) 
+u = "fnd -> " ++ imprimir aa ++ "   ->                                                       " ++ show( fnd aa)
+v = "bonita -> " ++ imprimir b ++ "  ->   " ++ bonita b
+w = "taut -> " ++ taut propTaut
 
-main = print (deMorgan aa)
+
+main = print (w)
    -- putStrLn "Hello World"  
